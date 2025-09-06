73342d2fda14c88030ccb468a1d0f8a9a86a149c
chocoie <chocoielzs@gmail.com>
2025-09-06 17:00:57 +0800

    fix m3u8 args order

--- script.sh
+++ script.sh
@@ -79,8 +79,8 @@ n_m3u8dl_re_record_stdout_no_url_no_format_partial_command=(
         '--live-keep-segments False'  # pipe to ffmpeg directly without saving segments
         '--no-ansi-color'
         '--auto-select'
-        '--no-log'
         '--log-level OFF'   # IGNORE std output logging from N_m3u8DL-RE (it will be mixed with ffmpeg output otherwise)
+        '--no-log'
 )
 
 # Add N_m3u8DL_RE_OPTIONS using eval to preserve quotes
